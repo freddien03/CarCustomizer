e25dfdbbfb9971d25e4f130879c47ddc814495db
freddien03 <60051731+freddien03@users.noreply.github.com>
2021-03-09 19:12:40 +0000

    Initial Commit

---
+++ CarCustomizer/CarCustomizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCustomizerApp.swift
+//  CarCustomizer
+//
+//  Created by Freddie Nunn on 09/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct CarCustomizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
